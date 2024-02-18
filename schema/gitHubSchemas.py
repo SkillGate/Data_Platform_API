6df48002638562666d94057574805b94d6cc779d
AshaniImalsha <ashaniimalsha26@gmail.com>
2024-02-18 11:26:29 +0530

    updated github commit count and details end point

--- schema/gitHubSchemas.py
+++ schema/gitHubSchemas.py
@@ -64,11 +64,22 @@ def extract_username_repo(github_url):
     return username, repo
 
 def get_contributors_commits_count(repositories_data):
-    return repositories_data
+    result = {"repositories": []}
+
+    for repo_name, contributors_data in repositories_data.items():
+        repo_info = {"reponame": repo_name, "contributors": []}
+
+        for contributor_name, commit_data in contributors_data.items():
+            contributor_info = {"username": contributor_name, "commit_count": commit_data['commit_count']}
+            repo_info["contributors"].append(contributor_info)
+
+        result["repositories"].append(repo_info)
+
+    return result
 
 def extract_contributors_commits_count(github_url, access_token):
     username, repo = extract_username_repo(github_url)
-    repositories_data = []
+    repositories_data = {}
 
     # Check if the provided URL is an organization link
     if repo == '':
@@ -78,7 +89,7 @@ def extract_contributors_commits_count(github_url, access_token):
 
     try:
         for repository in repositories:
-            repository_contributors_stats = []
+            repository_data = {}
 
             # Extract contributors and commit counts for each repository
             contributors = get_contributors(username, repository, access_token)
@@ -88,19 +99,15 @@ def extract_contributors_commits_count(github_url, access_token):
                     contributor_username = contributor['login']
                     commit_count = len(get_commits(username, repository, contributor_username, access_token))
 
-                    repository_contributors_stats.append({
-                        'username': contributor_username,
+                    repository_data[contributor_username]={
                         'commit_count': commit_count
-                    })
+                    }
                 else:
                     # Handle the case where 'login' key is not present or contributor is not a dictionary
                     print("Invalid contributor data:", contributor)
 
-            repository_data = {
-                'reponame': repository,
-                'commit_description': repository_contributors_stats
-            }
-            repositories_data.append(repository_data)
+            repositories_data[repository] = repository_data
+           
             
     except Exception as e:
         print(f"Error during iteration: {e}")
@@ -108,73 +115,72 @@ def extract_contributors_commits_count(github_url, access_token):
     return get_contributors_commits_count(repositories_data)
 
 
-def get_contributors_commits_details(contributors_stats):
-    data = {"contributors_commits": []}
+def get_contributors_commits_details(repositories_data):
+    data = {"repositories": []}
 
-    for contributor_username, stats in contributors_stats.items():
-        contributor_data = {"username": contributor_username, "commit_details": []}
+    for repository_name, contributors_data in repositories_data.items():
+        repository_info = {"reponame": repository_name, "contributors": []}
 
-        for commit_sha, commit_details in stats['commit_details'].items():
-            commit_name = commit_details['commit']['message']
-            stats = commit_details.get('stats', {})
-            lines = stats.get('total', 0)
+        for contributor_username, stats in contributors_data.items():
+            contributor_data = {
+                "username": contributor_username,
+                "commit_count": stats['commit_count'],
+                "commit_details": []
+            }
 
-            commit_data = {"commit_name": commit_name, "lines_changed": lines}
-            contributor_data["commit_details"].append(commit_data)
+            for commit_sha, commit_details in stats['commit_details'].items():
+                commit_name = commit_details['commit']['message']
+                stats = commit_details.get('stats', {})
+                lines_changed = stats.get('total', 0)
 
-        data["contributors_commits"].append(contributor_data)
+                commit_data = {
+                    "commit_name": commit_name,
+                    "lines_changed": lines_changed,
+                    "commit_sha": commit_sha
+                }
+                contributor_data["commit_details"].append(commit_data)
+
+            repository_info["contributors"].append(contributor_data)
+
+        data["repositories"].append(repository_info)
 
     return data
 
 def extract_contributors_commits_details(github_url, access_token):
     username, repo = extract_username_repo(github_url)
 
-    contributors_stats = {}
+    repositories_data = {}
 
     # Check if the provided URL is an organization link
     if repo == '':
         repositories = get_organization_repositories(username, access_token)
-
-        for repository in repositories:
-            # Extract contributors and commit details for each repository
-            contributors = get_contributors(username, repository, access_token)
-
-            for contributor in contributors:
-                contributor_username = contributor['login']
-                commits = get_commits(username, repository, contributor_username, access_token)
-
-                commit_details = {}
-
-                for commit in commits:
-                    commit_sha = commit['sha']
-                    commit_details[commit_sha] = get_commit_details(username, repository, commit_sha, access_token)
-
-                contributors_stats[contributor_username] = {
-                    'repository': repository,
-                    'commit_count': len(commits),
-                    'commit_details': commit_details,
-                }
     else:
-        # Extract contributors and commit details for the specified repository
-        contributors = get_contributors(username, repo, access_token)
+        repositories = [repo]
+
+    for repository in repositories:
+        repository_data = {}
+
+        # Extract contributors and commit details for each repository
+        contributors = get_contributors(username, repository, access_token)
 
         for contributor in contributors:
             contributor_username = contributor['login']
-            commits = get_commits(username, repo, contributor_username, access_token)
+            commits = get_commits(username, repository, contributor_username, access_token)
 
             commit_details = {}
 
             for commit in commits:
                 commit_sha = commit['sha']
-                commit_details[commit_sha] = get_commit_details(username, repo, commit_sha, access_token)
+                commit_details[commit_sha] = get_commit_details(username, repository, commit_sha, access_token)
 
-            contributors_stats[contributor_username] = {
-                'repository': repo,
+            repository_data[contributor_username] = {
                 'commit_count': len(commits),
                 'commit_details': commit_details,
             }
+        
+        repositories_data[repository] = repository_data
             
-    return get_contributors_commits_details(contributors_stats)
+    return get_contributors_commits_details(repositories_data)
 
 def github_collaborators_commit_count(gitHubUrl):
     github_repo_url = gitHubUrl 
