8bb7608d9a1e4b0c960134bd2d3decaf6fae04cd
AshaniImalsha <ashaniimalsha26@gmail.com>
2024-04-08 12:48:37 +0530

    chnage python version totally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from fastapi import FastAPI, __version__
+from fastapi import FastAPI
 from routes.route import router
 
 app = FastAPI()
-
-from pymongo.mongo_client import MongoClient
 
 app.include_router(router)
