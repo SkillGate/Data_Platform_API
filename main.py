fbceb69831a1417c7f8e975dc5fc114e55e3c361
AshaniImalsha <ashaniimalsha26@gmail.com>
2024-04-08 14:26:30 +0530

    update vercel configs

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
 from routes.route import router
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 app.include_router(router)
+
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            hello world
+        </div>
+    </body>
+</html>
+"""
+
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
