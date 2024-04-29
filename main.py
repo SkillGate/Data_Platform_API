3ac7749482cb41355732f4e954f3450cdc9f32ef
AshaniImalsha <ashaniimalsha26@gmail.com>
2024-04-29 19:41:16 +0530

    Update github projects data

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routes.route import router
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Adjust as needed for your setup
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
